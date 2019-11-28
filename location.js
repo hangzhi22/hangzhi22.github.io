7fc7e28c8b36b46b9b5610171f9b43abe944aff6
hangzhi <hangzhi22@sina.come>
2019-11-28 17:03:50 +0800

    update

--- location.js
+++ location.js
@@ -62,8 +62,8 @@ window.onload = () => {
         loadPlaces(position.coords)
             .then((places) => {
                 places.forEach((place) => {
-                    const latitude = place.location.lat + 0.000006;
-                    const longitude = place.location.lng+ 0.000006;
+                    const latitude = place.location.lat + 0.000002;
+                    const longitude = place.location.lng+ 0.000002;
 
                     // add place icon
                     const icon = document.createElement('a-image');
